38de21c6bfb5b2902045f05350ba84d41b3b8552
mc-gomes <bigmath2001@hotmail.com>
2021-07-22 12:40:33 -0300

    teste deleção de comentario lin 29

--- batalha.c
+++ batalha.c
@@ -24,7 +24,7 @@ void cabecalho(){
     tracinhos();}
 }
 
-int analise(char cam[LIM][LIM], int l, int c, int n) /// colocando comentario dnv
+int analise(char cam[LIM][LIM], int l, int c, int n)
 {   int val = 0;
     if((c-n) >= 0 && (c-n) <= 8){   /// à esquerda
         if(cam[l-1][c-n] == ' '){
